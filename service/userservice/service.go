8e4df4371a8aeb079b7d792b1bf3d2b7f04a121a
hasanmonfared <hasandoulabi@gmail.com>
2024-04-14 18:15:59 +0330

    Some change for run main.go

--- service/userservice/service.go
+++ service/userservice/service.go
@@ -1,13 +1,6 @@
 package userservice
 
-import (
-	"gameapp/entity"
-)
-
 type Repository interface {
-	Register(u entity.User) (entity.User, error)
-	GetUserByPhoneNumber(phoneNumber string) (entity.User, error)
-	GetUserByID(userID uint) (entity.User, error)
 }
 
 type Service struct {
--- validator/uservalidator/validator.go
+++ validator/uservalidator/validator.go
@@ -1,14 +1,10 @@
 package uservalidator
 
-import "gameapp/entity"
-
 const (
 	phoneNumberRegex = "^09[0-9]{9}$"
 )
 
 type Repository interface {
-	IsPhoneNumberUnique(phoneNumber string) (bool, error)
-	GetUserByPhoneNumber(phoneNumber string) (entity.User, error)
 }
 type Validator struct {
 	repo Repository
